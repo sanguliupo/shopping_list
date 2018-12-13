f0df378b76101cb9844888a424e223df4c808c73
Vanessa <44301380+sanguliupo@users.noreply.github.com>
2018-12-12 21:31:23 -0800

    optimize code

--- script.js
+++ script.js
@@ -9,12 +9,14 @@ function createListElement() {
 	var newLi = document.createElement('li');
 	// newLi.appendChild(document.createTextNode(input.value));
 	newLi.innerHTML = input.value;
-	newLi.title = 'list';
-	ul.appendChild(newLi);
+	newLi.classList.add('liText');
+
 	var button = document.createElement('button');
 	button.innerHTML = 'Delete';
 	button.classList.add('del');
 	newLi.appendChild(button);
+	// move this to end to have better performance
+	ul.appendChild(newLi);
 	input.value = '';
 }
 
@@ -34,24 +36,12 @@ function addListAfterKeypress(event) {
 }
 input.addEventListener('keypress', addListAfterKeypress);
 
-//
-//
-//
-//
-//CrossOffToggle
-function crossOffToggle(event) {
-	if (event.target.title == 'list') {
+function crossOffToggleOrDelete(event) {
+	if (event.target.className.includes('liText')) {
 		event.target.classList.toggle('done');
-	}
-}
-ul.addEventListener('click', crossOffToggle);
-//
-//
-// //Delete Event
-function deleteItem(event) {
-	if (event.target.className == 'del') {
+	} else if (event.target.className == 'del') {
 		event.target.parentNode.remove();
 	}
 }
 
-ul.addEventListener('click', deleteItem);
+ul.addEventListener('click', crossOffToggleOrDelete);
